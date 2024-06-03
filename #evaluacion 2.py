e490db7d5040abbfffd188264643308ea7d0e8e3
FelipePL2 <fpena2024@alu.uct.cl>
2024-06-03 09:10:40 -0400

    crear word

--- #evaluacion 2.py
+++ #evaluacion 2.py
@@ -1,2 +1,17 @@
-#evaluacion 2
-print("adios")
+#prueba
+from docx import Document
+import os
+
+power = Document()
+
+power.add_heading("Titulo", 0)
+
+filename = "word.docx"
+power.save(filename)
+
+if os.name == 'nt':
+    os.startfile(filename)
+elif os.name == 'posix':
+    os.system(f'open {filename}')
+else:
+    print(f"No se puede abrir automáticamente el archivo en este sistema operativo: {os.name}")
